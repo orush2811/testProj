0e81ebe857ac80f55e1e43e92631f405b48efaf9
Or Itach <orwolf132@gmail.com>
2024-05-05 15:51:45 +0300

    Added displayArray

--- sumArray.C
+++ sumArray.C
@@ -20,6 +20,13 @@ int searchEleInArray(int *ptr,int size,int ele){
     return 0;
 }
 
+void displayArray(int *ptr, int size){
+
+    for (int i=0;i<size;i++){
+        printf("Value #%d is %d\n",i+1,ptr[i]);
+    }
+}
+
 int findMaxValue(int *ptr, int size){
     int max=0;
     for(int i=0;i<SIZE;i++){
@@ -36,6 +43,7 @@ int main(){
     int grades[3]={3,5,7},result=0,result2=0,key,maxvalue;
     int grades2[4]={80,85,72,90};
     result=sumArray(grades,3);
+    displayArray(grades2,4);
     printf("Enter the desired element you would like to search");
     scanf("%d",&key);
     result2=searchEleInArray(grades2,SIZE,key);
