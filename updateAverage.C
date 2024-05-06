f7e02d374fe83d6d8d882c30db74473982656128
Or Itach <orwolf132@gmail.com>
2024-05-06 14:27:58 +0300

    corrected the neighbouring function

--- updateAverage.C
+++ updateAverage.C
@@ -2,28 +2,37 @@
 #include <stdlib.h>
 #include <string.h>
 
-void updateAverage(int num1, int num2, float *avg){
-   *avg=(num1+num2)/2.0;
-    
+void updateAverage(int num1, int num2, float *avg)
+{
+    *avg = (num1 + num2) / 2.0;
 }
 
-int sumNeighbours(int *ptr, int size){
+int sumNeighbours(int *ptr, int size)
+{
     int sum;
-    for(int i=0;i<size;i++){
-        if(ptr[i+1] == ptr[i] + ptr[i+2])
-        return 1;
+    for (int i = 1; i < size - 1; i++)
+    {
+        if (ptr[i] == ptr[i - 1] + ptr[i + 1])
+            return 1;
     }
     return 0;
 }
 
-int main(){
-    int a,b,send;
+int main()
+{
+    int a, b, send;
     float average;
+    int elements[5] = {1, 4, 7, 3, 2}; // 1,4,1,4,2 for bad if needed
     printf("Enter the first number:\n");
-    scanf("%d",&a);
+    scanf("%d", &a);
     printf("Enter the second number:\n");
-    scanf("%d",&b);
-    updateAverage(a,b,&average);
-    printf("The average of both numbers is %f\n",average);
+    scanf("%d", &b);
+    updateAverage(a, b, &average);
+    send = sumNeighbours(elements, 5);
+    printf("The average of both numbers is %f\n", average);
+    if (send == 1)
+        printf("The sum of the neighbouring elements is good", send);
+    if (send == 0)
+        printf("The sum of the neighbouring elements is bad", send);
     return 0;
 }
