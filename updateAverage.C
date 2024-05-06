624228de73096266a9ccd9da9b8318406439bc4f
Or Itach <orwolf132@gmail.com>
2024-05-06 14:18:21 +0300

    Added sumNeighbours

--- updateAverage.C
+++ updateAverage.C
@@ -7,15 +7,29 @@ void updateAverage(int num1, int num2, float *avg){
     
 }
 
+int sumNeighbours(int *ptr, int size){
+    int sum;
+    for(int i=0;i<size;i++){
+        if(ptr[i+1] == ptr[i] + ptr[i+2])
+        return 1;
+    }
+    return 0;
+}
 
 int main(){
-    int a,b;
+    int a,b,send;
     float average;
+    int elements[5]={1,4,7,3,2};
     printf("Enter the first number:\n");
     scanf("%d",&a);
     printf("Enter the second number:\n");
     scanf("%d",&b);
     updateAverage(a,b,&average);
+    send = sumNeighbours(elements, 5);
     printf("The average of both numbers is %f\n",average);
+    if(send==1)
+    printf("The sum of the neighbouring elements is good",send);
+    if(send==0)
+    printf("The sum of the neighbouring elements is bad",send);
     return 0;
 }
