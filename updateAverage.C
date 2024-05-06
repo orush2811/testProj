4ac44c22be075e4e93be3b6c9d7231e2760ba682
Or Itach <orwolf132@gmail.com>
2024-05-06 19:44:25 +0300

    Added the sortOrNot function

--- updateAverage.C
+++ updateAverage.C
@@ -18,21 +18,38 @@ int sumNeighbours(int *ptr, int size)
     return 0;
 }
 
+int sortedOrNot(int *ptr, int size, int *result){
+  *result=1; // Assuming the array is really sorted
+  for(int i=1;i<size;i++){
+    if(ptr[i]<=ptr[i-1])
+    *result=0; // The array is not sorted
+    if(ptr[i]<ptr[i-1])
+    return 0;
+  }
+  return 1;
+}
+
 int main()
 {
-    int a, b, send;
+    int a, b, send,sort,check;
     float average;
-    int elements[5] = {1, 4, 7, 3, 2}; // 1,4,1,4,2 for bad if needed
+    int elements[5] = {1, 4, 7, 3, 2}; // 1,4,1,4,2 for bad if needed,1,4,7,3,2 is the good array, 1,2,3,4,5 to check for the sortedOrNot function
     printf("Enter the first number:\n");
     scanf("%d", &a);
     printf("Enter the second number:\n");
     scanf("%d", &b);
     updateAverage(a, b, &average);
     send = sumNeighbours(elements, 5);
+    sort=sortedOrNot(elements,5,&check);
+    if(sort == 0)
+    printf("The array is not sorted!\n",sort);
+    if(sort == 1)
+    printf("The array is sorted!\n",sort);
     printf("The average of both numbers is %f\n", average);
     if (send == 1)
         printf("The sum of the neighbouring elements is good", send);
     if (send == 0)
         printf("The sum of the neighbouring elements is bad", send);
     return 0;
+
 }
