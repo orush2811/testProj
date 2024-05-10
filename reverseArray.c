a22746f1f7b354ba5405a052e35b11e9c05c11f4
Or Itach <orwolf132@gmail.com>
2024-05-10 10:02:49 +0300

    Added reverseArray function with recursive option

--- reverseArray.c
+++ reverseArray.c
@@ -15,11 +15,32 @@ void printArr(char *arr,int size){
    for(int i=0;i<size;i++){
     printf("%c ",arr[i]);
    }
+   printf("\n");
+}
+
+void reverseArrIterative(char *arr, int size){
+   for(int i=0;i<size/2;i++){
+      swap(&arr[i],&arr[size-1-i]);
+   }
+}
+
+void reverseArrRecursive(char *arr, int size){
+   if(size>1){
+      swap(&arr[0],&arr[size-1]); // Can also use swap(arr,arr+size-1)
+      reverseArrRecursive(arr+1,size-2);
+   }
+
 }
 
 int main(){
     
     char arr[SIZE]={'a','f','k','d'};
+    printf("The array of characters before swapping:\n");
+    printArr(arr,SIZE);
+    reverseArrRecursive(arr,SIZE);
+    printf("The array of characters after swapping:\n");
+    printArr(arr,SIZE);
+
     
     return 0;
 }
