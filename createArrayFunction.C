1c848a7c4ceb5a9697b137ce850eee4d2b83dfec
Or Itach <orwolf132@gmail.com>
2024-05-15 13:34:13 +0300

    Added createArrayFunction

---
+++ createArrayFunction.C
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define SIZE 5
+
+void printArr(int *arr, int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+int main()
+{
+
+    int *arr;
+    arr = createArray();
+    printArr(arr,SIZE);
+}
+
+int *createArray()
+{
+    int i;
+    int myArr[SIZE];
+    printf("Enter %d elements into you array");
+    for (i = 0; i < SIZE; i++)
+    {
+        printf("Enter number: %d ", i);
+        scanf("%d", &myArr[i]);
+    }
+    return myArr;
+}
