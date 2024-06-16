eda404d2820edae3614036527c9fe34329050e8f
Or <orwolf132@gmail.com>
2024-06-16 09:46:07 +0300

    Added malloc example

--- malloc+calloc.C
+++ malloc+calloc.C
@@ -2,22 +2,30 @@
 #include <stdlib.h>
 #include <string.h>
 
+int DynamicArray(int *ptr)
+{
 
-int DynamicArray(int *ptr){
-
-    ptr=(int *)malloc(5*sizeof(int));
-    for(int i=0;i<5;i++){
-        ptr[i]=i+1;
-        printf("Arr in position %d is %d",i,ptr[i]);
+    ptr = (int *)malloc(5 * sizeof(int));
+    if (ptr == NULL)
+    {
+        printf("There was an error while trying to allocate the memory");
+        exit(EXIT_FAILURE);
+    }
+    printf("Memory allocation successful\n");
+    for (int i = 0; i < 5; i++)
+    {
+        ptr[i] = i + 1;
+        printf("Arr in position %d is %d", i, ptr[i]);
         printf("\n");
     }
 
     return *ptr;
+    free(ptr);
 }
 
-
-int main(){
+int main()
+{
     int arr[5];
-    int x=DynamicArray(arr);
+    int x = DynamicArray(arr);
     return 0;
 }
